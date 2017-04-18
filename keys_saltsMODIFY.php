b5e9a21bfa1603e4cef36af97b00305368feb435
jimbursch <jim@jimbursch.com>
2017-04-18 15:21:43 -0700

    added keys_saltsMODIFY

---
+++ keys_saltsMODIFY.php
@@ -0,0 +1,17 @@
+<?php
+/*
+
+There are three changes that need to be made to this file:
+
+1. Replace "your-key-here" with your own CryptoID API key
+2. Replace "your-random-string-here" with your own random string of characters
+3. Rename this file to keys_salts.php
+
+*/
+
+
+define('CRYPTOID_API_KEY','your-key-here');
+
+define('INVOICE_HASH_SALT','your-random-string-here')
+
+?>
