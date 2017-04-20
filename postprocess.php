182c97bf65b2538b5bd7eeb89dc6bda74aab842f
jimbursch <jim@jimbursch.com>
2017-04-20 07:29:52 -0700

    added comments

--- postprocess.php
+++ postprocess.php
@@ -2,12 +2,16 @@
 
 session_start();
 
+
+// ideally for security purposes this include should reside outside the web directory. IF you move it, this include needs to be updated with the new location.
 include('keys_salts.php');
 
 $error='';
 
 
-// create an array of the urlencoded variables, also construct the GET query string, and the concatenated invoice
+// create an array of the urlencoded variables
+// construct the GET query string
+// concatenate the invoice data for the invoice hash
 $enc=array();
 $querystrng='';
 $concatenated='';
@@ -20,15 +24,17 @@ foreach ($_POST as $key => $value) {
 }// end if ($_POST) 
 
 
-
+// error checking
 if (!is_numeric($_POST['amount'])) {$error.='<p>Please enter a number for the amount.</p>';}
 if (empty($_POST['addr'])) {$error.='<p>Please enter valid Dash address where you would like to receive your Dash.</p>';}
 
+
+// salt the invoice data with your unique salt, hash it, and add it to the query string
 $salted=INVOICE_HASH_SALT.$concatenated;
 $invoice_hash=hash('sha256',$salted);
 $querystring='?inv_hash='.$invoice_hash.$querystring;
 
-
+// if there is no error, redirect to index with query string (this is the invoice) else redirect to index with error message in $_SESSION
 if (!$error) {$url='index.php'.$querystring;} else {$url='index.php';$_SESSION['message']=$error;}
 
 
